5b82a7f7e2c5a3083c2d3a1bdab4c28b16ec36a2
lovro <lovro.zagar5@gmail.com>
2023-05-17 16:19:27 -0400

    feat: add route and controller for forecast weather

--- controllers/weather/forecastController.js
+++ controllers/weather/forecastController.js
@@ -1,20 +1,18 @@
 const fetch = require('cross-fetch')
 
-exports.getCurrentWeatherData = async (req, res) => {
+exports.getForecastData = async (req, res) => {
   const { latitude, longitude } = req.query
 
   const prefix = 'https://api.open-meteo.com/v1/forecast?'
   const lat = `latitude=${latitude}`
   const lng = `&longitude=${longitude}`
   const options =
-    '&current_weather=true&hourly=temperature_2m,windspeed_10m,weathercode'
+    '&current_weather=true&hourly=relativehumidity_2m,temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max&timezone=auto'
 
   try {
     const response = await fetch(`${prefix}${lat}${lng}${options}`)
     const json = await response.json()
-    const { current_weather } = json
-
-    res.json(current_weather)
+    res.json(json)
     //
   } catch (error) {
     res.status(500).json({ error: 'An error occurred' })
--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const currentController = require('../controllers/weather/currentController')
+const forecastController = require('../controllers/weather/forecastController')
 
 const router = express.Router()
 
 router.get('/current', currentController.getCurrentWeatherData)
+router.get('/forecast', forecastController.getForecastData)
 
 module.exports = router
